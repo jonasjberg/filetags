09c7c73bb6b5c082fe3d45430be29f3dd16e38de
Jonas Sjöberg <jomeganas@gmail.com>
2016-04-01 15:08:34 +0200

    Add modified wrapper scripts.
    Adds modified versions of the wrapper scripts.
    
    * Should work regardless of what the terminal binary is called.
    * Converts a relative path to the 'filetags.py' file to a absolute path
      and checks whether the conversion was successful.

---
+++ geeqie/vk-filetags-interactive-removing-wrapper.sh
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env bash
+# Modified wrapper for filetags
+
+# Get absolute path
+FILETAGS=$(readlink -f ../filetags.py)
+[ -x "$FILETAGS" ] || { echo "Resolve path failed. Exiting." 2>&1 ; exit 1 ; }
+
+
+/usr/bin/x-terminal-emulator \
+    --geometry=73x5+330+5    \
+    --hide-menubar           \
+    --hide-toolbar           \
+    --execute "$FILETAGS" --interactive --remove "${@}"
+
+#end
