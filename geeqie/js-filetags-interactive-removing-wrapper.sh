b390ba72d647583dfef334557fb88ac50629e816
Jonas Sjöberg <jomeganas@gmail.com>
2017-03-18 21:47:13 +0100

    Wrap variable in quotes.

--- geeqie/js-filetags-interactive-removing-wrapper.sh
+++ geeqie/js-filetags-interactive-removing-wrapper.sh
@@ -2,7 +2,7 @@
 # Modified wrapper for filetags
 
 # Get absolute path
-SCRIPT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+SCRIPT_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$SCRIPT_PATH" && FILETAGS=$(readlink -f "../filetags.py") 
 [ -x "$FILETAGS" ] || { echo "Resolve path failed. Exiting." 2>&1 ; exit 1 ; }
 
